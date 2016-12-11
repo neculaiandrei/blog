3be42347f2f0880fd09069f390d4b49da55ffcd2
Neculai Andrei <an_drey_curent@yahoo.com>
2016-12-11 12:11:42 +0200

    - improve state reducer after edit.

--- src/app/scripts/reducers/postsReducer.js
+++ src/app/scripts/reducers/postsReducer.js
@@ -5,11 +5,14 @@ export default function postsReducer(state = InitialState.posts, action) {
   switch (action.type) {
     case type.CREATE_POST_SUCCESS:
       return [...state, Object.assign({}, action.post)];
-    case type.EDIT_POST_SUCCESS:
+    case type.EDIT_POST_SUCCESS: {
+      const postIndex = state.findIndex(post => post._id === action.post._id);
       return [
-        ...state.filter(post => post._id !== action.post._id),
+        ...state.slice(0, postIndex),
         Object.assign({}, action.post),
+        ...state.slice(postIndex + 1),
       ];
+    }
     case type.DELETE_POST_SUCCESS:
       return [...state.filter(post => post._id !== action.post._id)];
     case type.LOAD_POSTS_SUCCESS:
